0246b36adbd314167a39e9dd10721eba90289574
elvisgu <1016676900@qq.com>
2020-06-08 18:32:59 +0800

    将所有的样式改为本地样式

--- examples/00_template/xgui/xgui_page_home.c
+++ examples/00_template/xgui/xgui_page_home.c
@@ -1,5 +1,4 @@
 #include "xgui.h"
-#include "lv_port_indev.h"
 
 static lv_obj_t * add_icon(lv_obj_t * parent, const void * src, const char * txt);
 static void led_event_cb(lv_obj_t * obj, lv_event_t event);
@@ -14,65 +13,57 @@ static void focus_cb(lv_group_t * g);
 void xgui_page_home(void)
 {
     LV_IMG_DECLARE(light);
+    LV_IMG_DECLARE(buzzer);
 
-    lv_group_set_focus_cb(xgui_group, focus_cb);
+    lv_group_set_focus_cb(xgui_group, focus_cb); /* 聚焦回调，用于将聚焦对象移动到屏幕可视范围内 */
 
     lv_obj_t * cont = lv_page_create(lv_scr_act(), NULL);
     lv_page_set_scrl_layout(cont, LV_LAYOUT_PRETTY_MID);
     lv_obj_set_size(cont, LV_HOR_RES, LV_VER_RES);
 
-    lv_obj_set_style_local_pad_left(lv_page_get_scrllable(cont), LV_IMG_PART_MAIN, LV_STATE_DEFAULT, LV_DPX(60));
-    lv_obj_set_style_local_pad_right(lv_page_get_scrllable(cont), LV_IMG_PART_MAIN, LV_STATE_DEFAULT, LV_DPX(60));
-    lv_obj_set_style_local_pad_top(lv_page_get_scrllable(cont), LV_IMG_PART_MAIN, LV_STATE_DEFAULT, LV_DPX(60));
-    lv_obj_set_style_local_pad_bottom(lv_page_get_scrllable(cont), LV_IMG_PART_MAIN, LV_STATE_DEFAULT, LV_DPX(60));
-    lv_obj_set_style_local_pad_inner(lv_page_get_scrllable(cont), LV_IMG_PART_MAIN, LV_STATE_DEFAULT, LV_DPX(100));
+    /* 设置背景梯度 */
+    lv_obj_set_style_local_bg_grad_color(cont, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_PURPLE);
+    lv_obj_set_style_local_bg_grad_dir(cont, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, LV_GRAD_DIR_VER);
+    lv_obj_set_style_local_bg_main_stop(cont, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, 0);
+    lv_obj_set_style_local_bg_grad_stop(cont, LV_CONT_PART_MAIN, LV_STATE_DEFAULT, 255);
+
+    lv_obj_set_style_local_pad_left(lv_page_get_scrllable(cont), LV_CONT_PART_MAIN, LV_STATE_DEFAULT, LV_DPX(50));
+    lv_obj_set_style_local_pad_right(lv_page_get_scrllable(cont), LV_CONT_PART_MAIN, LV_STATE_DEFAULT, LV_DPX(50));
+    lv_obj_set_style_local_pad_top(lv_page_get_scrllable(cont), LV_CONT_PART_MAIN, LV_STATE_DEFAULT, LV_DPX(50));
+    lv_obj_set_style_local_pad_bottom(lv_page_get_scrllable(cont), LV_CONT_PART_MAIN, LV_STATE_DEFAULT, LV_DPX(50));
+    lv_obj_set_style_local_pad_inner(lv_page_get_scrllable(cont), LV_CONT_PART_MAIN, LV_STATE_DEFAULT, LV_DPX(100));
 
     lv_obj_t *icon = add_icon(cont, &light, "LED");
-    // lv_obj_align_origo(icon, NULL, LV_ALIGN_IN_TOP_MID, 0, 10);
     lv_obj_set_event_cb(icon, led_event_cb);
     lv_group_add_obj(xgui_group, icon);
 
     icon = add_icon(cont, &light, "BTN");
-    // lv_obj_align_origo(icon, NULL, LV_ALIGN_IN_TOP_MID, 0, 105);
+    lv_obj_set_event_cb(icon, led_event_cb);
+    lv_group_add_obj(xgui_group, icon);
+
+    icon = add_icon(cont, &buzzer, "beep");
     lv_obj_set_event_cb(icon, led_event_cb);
     lv_group_add_obj(xgui_group, icon);
 
     icon = add_icon(cont, &light, "beep");
-    // lv_obj_align_origo(icon, NULL, LV_ALIGN_IN_TOP_MID, 0, 200);
     lv_obj_set_event_cb(icon, led_event_cb);
     lv_group_add_obj(xgui_group, icon);
 
     icon = add_icon(cont, &light, "beep");
-    // lv_obj_align_origo(icon, NULL, LV_ALIGN_IN_TOP_MID, 0, 200);
     lv_obj_set_event_cb(icon, led_event_cb);
     lv_group_add_obj(xgui_group, icon);
 
     icon = add_icon(cont, &light, "beep");
-    // lv_obj_align_origo(icon, NULL, LV_ALIGN_IN_TOP_MID, 0, 200);
     lv_obj_set_event_cb(icon, led_event_cb);
     lv_group_add_obj(xgui_group, icon);
 
     icon = add_icon(cont, &light, "beep");
-    // lv_obj_align_origo(icon, NULL, LV_ALIGN_IN_TOP_MID, 0, 200);
     lv_obj_set_event_cb(icon, led_event_cb);
     lv_group_add_obj(xgui_group, icon);
 
     icon = add_icon(cont, &light, "beep");
-    // lv_obj_align_origo(icon, NULL, LV_ALIGN_IN_TOP_MID, 0, 200);
     lv_obj_set_event_cb(icon, led_event_cb);
     lv_group_add_obj(xgui_group, icon);
-
-    icon = add_icon(cont, &light, "beep");
-    // lv_obj_align_origo(icon, NULL, LV_ALIGN_IN_TOP_MID, 0, 200);
-    lv_obj_set_event_cb(icon, led_event_cb);
-    lv_group_add_obj(xgui_group, icon);
-
-    lv_obj_t *btn = lv_btn_create(cont, NULL);
-    lv_group_add_obj(xgui_group, btn);
-    btn = lv_btn_create(cont, NULL);
-    lv_group_add_obj(xgui_group, btn);
-    btn = lv_btn_create(cont, NULL);
-    lv_group_add_obj(xgui_group, btn);
 }
 
 /**
@@ -82,18 +73,9 @@ void xgui_page_home(void)
   */
 void xgui_page_home_back(void)
 {
-    static lv_style_t back_style;
-    lv_style_reset(&back_style);
-    lv_style_init(&back_style);
-
-    lv_style_set_bg_opa(&back_style, LV_STATE_DEFAULT, LV_OPA_TRANSP);
-    lv_style_set_radius(&back_style, LV_STATE_DEFAULT, 20);
-    lv_style_set_value_str(&back_style, LV_STATE_DEFAULT, LV_SYMBOL_LEFT);
-
     /* 返回键 */
     lv_obj_t *back = lv_btn_create(lv_scr_act(), NULL);
     lv_obj_set_size(back, 40, 40);
-    // lv_obj_add_style(back, LV_OBJ_PART_MAIN, &back_style);
     lv_obj_align(back, NULL, LV_ALIGN_IN_BOTTOM_LEFT, 10, 0);
     lv_obj_set_event_cb(back, back_event_cb);
     lv_group_add_obj(xgui_group, back);
@@ -104,32 +86,20 @@ void xgui_page_home_back(void)
 }
 
 
-static lv_obj_t * add_icon(lv_obj_t * parent, const void * src,
-        const char * txt) {
+static lv_obj_t * add_icon(lv_obj_t * parent, const void * src, const char * txt) {
+    
     lv_obj_t * icon = lv_img_create(parent, NULL);
-    // lv_theme_apply(icon, LV_DEMO_PRINTER_THEME_ICON);
     lv_img_set_src(icon, src);
     lv_obj_set_click(icon, true);
     lv_img_set_antialias(icon, false);
-    // lv_obj_set_size(icon, 100, 100);
 
-    lv_obj_set_style_local_value_ofs_y(icon, LV_IMG_PART_MAIN, LV_STATE_DEFAULT, 30);
-    lv_obj_set_style_local_value_str(icon, LV_IMG_PART_MAIN, LV_STATE_DEFAULT, txt);
-    lv_obj_set_style_local_pad_left(icon, LV_IMG_PART_MAIN, LV_STATE_DEFAULT, LV_DPX(40));
-    lv_obj_set_style_local_pad_right(icon, LV_IMG_PART_MAIN, LV_STATE_DEFAULT, LV_DPX(40));
-    lv_obj_set_style_local_pad_top(icon, LV_IMG_PART_MAIN, LV_STATE_DEFAULT, LV_DPX(40));
-    lv_obj_set_style_local_pad_bottom(icon, LV_IMG_PART_MAIN, LV_STATE_DEFAULT, LV_DPX(40));
-    lv_obj_set_style_local_pad_inner(icon, LV_IMG_PART_MAIN, LV_STATE_DEFAULT, LV_DPX(10));
-    lv_obj_set_style_local_outline_width(icon, LV_IMG_PART_MAIN, LV_STATE_DEFAULT, 3);
-    lv_obj_set_style_local_outline_opa(icon, LV_IMG_PART_MAIN, LV_STATE_DEFAULT, LV_OPA_0);
-    lv_obj_set_style_local_outline_opa(icon, LV_IMG_PART_MAIN, LV_STATE_FOCUSED, LV_OPA_50);
-    lv_obj_set_style_local_outline_color(icon, LV_IMG_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_RED);
-    lv_obj_set_style_local_outline_color(icon, LV_IMG_PART_MAIN, LV_STATE_EDITED, LV_COLOR_BLUE);
-    lv_obj_set_style_local_border_width(icon, LV_IMG_PART_MAIN, LV_STATE_DEFAULT, 3);
-    lv_obj_set_style_local_border_color(icon, LV_IMG_PART_MAIN, LV_STATE_FOCUSED, LV_COLOR_RED);
-    lv_obj_set_style_local_border_opa(icon, LV_IMG_PART_MAIN, LV_STATE_FOCUSED, LV_OPA_50);
-    lv_obj_set_style_local_border_opa(icon, LV_IMG_PART_MAIN, LV_STATE_DEFAULT, LV_OPA_TRANSP);
-    lv_obj_set_style_local_size(icon, LV_IMG_PART_MAIN, LV_STATE_DEFAULT, 100);
+    lv_obj_set_style_local_value_ofs_y(icon, LV_IMG_PART_MAIN, LV_STATE_DEFAULT, 30); /* 设置值在默认状态下的y轴偏移 */
+    lv_obj_set_style_local_value_str(icon, LV_IMG_PART_MAIN, LV_STATE_DEFAULT, txt); /* 设置默认状态下的值文本 */
+    lv_obj_set_style_local_outline_pad(icon, LV_IMG_PART_MAIN, LV_STATE_DEFAULT, LV_DPX(40)); /* 设置轮廓在默认状态下与对象的距离 */
+    lv_obj_set_style_local_outline_width(icon, LV_IMG_PART_MAIN, LV_STATE_DEFAULT, 3); /* 设置轮廓在默认状态下的宽度 */
+    lv_obj_set_style_local_outline_opa(icon, LV_IMG_PART_MAIN, LV_STATE_DEFAULT, LV_OPA_0); /* 设置轮廓在默认状态下的透明度 */
+    lv_obj_set_style_local_outline_opa(icon, LV_IMG_PART_MAIN, LV_STATE_FOCUSED, LV_OPA_50); /* 设置轮廓在聚焦状态下的透明度 */
+    lv_obj_set_style_local_outline_color(icon, LV_IMG_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_RED); /* 设置轮廓在默认状态下的颜色 */
 
     return icon;
 }
--- examples/00_template/xgui/xgui_page_led.c
+++ examples/00_template/xgui/xgui_page_led.c
@@ -1,5 +1,4 @@
 #include "xgui.h"
-#include "lv_port_indev.h"
 
 #include <rtdevice.h>
 #include <board.h>
@@ -24,10 +23,6 @@ static void sw_event_cb(lv_obj_t * obj, lv_event_t event);
   */
 void xgui_page_led(void)
 {
-    lv_group_t *group = lv_port_indev_get_group();
-
-    static lv_style_t style[LED_NUM];
-
     lv_color_t color[LED_NUM] = {LV_COLOR_RED, LV_COLOR_GREEN, LV_COLOR_BLUE};
 
     /* 配置LED引脚模式 */
@@ -40,27 +35,21 @@ void xgui_page_led(void)
 
     for(int i = 0; i < LED_NUM; i++)
     {
-        /* 初始化LED的样式 */
-        lv_style_reset(&style[i]);
-        lv_style_init(&style[i]);
-
-        /* 设置LED样式 */
-        lv_style_set_bg_color(&style[i], LV_STATE_DEFAULT, color[i]);
-        lv_style_set_border_color(&style[i], LV_STATE_DEFAULT, color[i]);
-        lv_style_set_shadow_color(&style[i], LV_STATE_DEFAULT, color[i]);
-
         /* 创建LED对象 */
         led[i]  = lv_led_create(lv_scr_act(), NULL);
         lv_obj_align(led[i], NULL, LV_ALIGN_IN_TOP_MID, -80 + i * 80, 10);
         lv_led_off(led[i]);
-        lv_obj_add_style(led[i], LV_OBJ_PART_MAIN, &style[i]);
+
+        lv_obj_set_style_local_bg_color(led[i], LV_LED_PART_MAIN, LV_STATE_DEFAULT, color[i]);
+        lv_obj_set_style_local_border_color(led[i], LV_LED_PART_MAIN, LV_STATE_DEFAULT, color[i]);
+        lv_obj_set_style_local_shadow_color(led[i], LV_LED_PART_MAIN, LV_STATE_DEFAULT, color[i]);
 
         /* 创建切换开关，用于控制LED */
         sw[i] = lv_switch_create(lv_scr_act(), NULL);
         lv_switch_off(sw[i], LV_ANIM_OFF);
         lv_obj_set_event_cb(sw[i], sw_event_cb);
         lv_obj_align(sw[i], led[i], LV_ALIGN_OUT_BOTTOM_MID, 0, 50);
-        lv_group_add_obj(group, sw[i]);
+        lv_group_add_obj(xgui_group, sw[i]);
     }
 
     xgui_page_home_back();
